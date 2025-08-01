944f3bf0a009eb23623d710db4c007b674d1b8a1
elisahardian <elisahardian005@gmail.com>
2025-08-01 15:49:24 +0700

    feat: import navbar component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Navbar from "./components/navbar";
+
 export default function Home() {
   return (
     <div className="font-sans flex flex-col gap-4 items-center justify-center align-middle w-full h-screen">
-      <Image
-        src="/stiraparadise-logo.jpeg"
-        alt="stira paradise logo"
-        width={128}
-        height={128}
-      />
-      <p className="w-96 text-center md:text-2xl">
-        Coming Soon Surga Tropis Stira ParADIse Resort
-      </p>
-      <Link
-        href="https://www.instagram.com/stiraparadise_resort"
-        className="underline text-blue-500"
-      >
-        Kunjungi Instagram Kami
-      </Link>
+      {/* navbar */}
+      <Navbar />
+
+      <div className="flex flex-col items-center mt-10">
+        <Image src="/stiraparadise-logo.jpeg" alt="stira paradise logo" width={128} height={128} />
+        <p className="w-96 text-center md:text-2xl">Coming Soon Surga Tropis Stira ParADIse Resort</p>
+        <Link href="https://www.instagram.com/stiraparadise_resort" className="underline text-blue-500">
+          Kunjungi Instagram Kami
+        </Link>
+      </div>
     </div>
   );
 }
