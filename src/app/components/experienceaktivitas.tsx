d7d0f7804e67783b4e9d544188b4689f18e44a61
elisahardian <elisahardian005@gmail.com>
2025-08-15 13:56:20 +0700

    style: improve UI consistency

--- src/app/components/experienceaktivitas.tsx
+++ src/app/components/experienceaktivitas.tsx
@@ -9,51 +9,51 @@ const ExperienceAktivitas = () => {
         {/* aktivitas */}
         <div className="flex flex-col items-center gap-2 ">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/sunsetview-exp.jpg" alt="Sunset View Point di tepi pantai" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/sunsetview-exp.jpg" alt="Sunset View Point di tepi pantai" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Sunset View Point di tepi pantai</p>
         </div>
         <div className="flex flex-col items-center gap-2">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/joggingtrack-exp.jpg" alt="Jogging track, Volley Beach & Sunrise Walk" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/joggingtrack-exp.jpg" alt="Jogging track, Volley Beach & Sunrise Walk" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Jogging track, Volley Beach & Sunrise Walk</p>
         </div>
         <div className="flex flex-col items-center gap-2 ">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/apiunggun-exp.jpg" alt="Api Unggun & Etnik Cinema" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/apiunggun-exp.jpg" alt="Api Unggun & Etnik Cinema" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Api Unggun & Etnik Cinema</p>
         </div>
         <div className="flex flex-col items-center gap-2">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/livemusic-exp.jpg" alt="Live Music & Gathering Eksklusif" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/livemusic-exp.jpg" alt="Live Music & Gathering Eksklusif" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Live Music & Gathering Eksklusif</p>
         </div>
         <div className="flex flex-col items-center gap-2 ">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/divesnorkling-exp.jpg" alt="Dive & Snorkling Discovery (coming soon)" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/divesnorkling-exp.jpg" alt="Dive & Snorkling Discovery (coming soon)" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Dive & Snorkling Discovery (coming soon)</p>
         </div>
         <div className="flex flex-col items-center gap-2">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/spamassage-exp.jpg" alt="Spa & Massage For Healthness" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/spamassage-exp.jpg" alt="Spa & Massage For Healthness" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Spa & Massage For Healthness</p>
         </div>
         <div className="flex flex-col items-center gap-2 ">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/turumkm-exp.jpg" alt="Tur UMKM dan kerajinan khas Pinrang (coming soon)" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/turumkm-exp.jpg" alt="Tur UMKM dan kerajinan khas Pinrang (coming soon)" width={200} height={150} className="object-cover w-full h-full" />
           </div>
           <p className=" italic text-sm md:text-base xl:text-xl ">Tur UMKM dan kerajinan khas Pinrang (coming soon)</p>
         </div>
         <div className="flex flex-col items-center gap-2">
           <div className="w-[200] h-[150] overflow-hidden rounded-[10px]">
-            <Image src="/sundaymarket-exp.jpg" alt="Sunday Market dengan produk lokal (comming soon)" width={250} height={150} className="object-cover w-full h-full" />
+            <Image src="/sundaymarket-exp.jpg" alt="Sunday Market dengan produk lokal (comming soon)" width={200} height={150} className="object-cover w-full h-full" />
           </div>
-          <p className=" italic text-sm md:text-base xl:text-xl ">Sunday Market dengan produk lokal (comming soon)</p>
+          <p className=" italic text-sm md:text-base xl:text-xl ">Sunday Market dengan produk lokal (coming soon)</p>
         </div>
       </div>
     </div>
